b4f680afb9dd8a6c131f8c6357c2dd66c7098276
NeverStop-lin <842546605@qq.com>
2020-11-17 21:30:01 +0800

    ;

--- .laya/cmpileExtra.js
+++ .laya/cmpileExtra.js
@@ -1,8 +1,7 @@
 
 (function () {
-    let texts = []
     'use strict';
-
+    let texts = [];
     const useIDENode = process.argv[0].indexOf("LayaAir") > -1 ? true : false;
     const useCMDNode = process.argv[1].indexOf("layaair2-cmd") > -1 ? true : false;
 
@@ -19,42 +18,41 @@
 
     const FS = require('fs');
     var path = require('path');
-    var exec = require('child_process').exec;
 
-    createViewScript()
+    createViewScript();
     /** 根据bin/Views/目录下的界面json文件 创建对应的界面class在src/Views/ */
     function createViewScript() {
         //获取所有json路径
-        let jsonPath = getFilePath(binPath + "\\Views")
+        let jsonPath = getFilePath(binPath + "\\Views");
         //获取所有界面class文件路径
-        let classPath = getFilePath(srcPath + "\\Views")
+        let classPath = getFilePath(srcPath + "\\Views");
         //需要创建的class文件路径
-        let needClassPath = []
+        let needClassPath = [];
         // 比较缺少哪些界面class 获取所要创建文件的路径数组
         jsonPath.relative.forEach(json_path => {
-            json_path = json_path.replace(".json", ".ts")
+            json_path = json_path.replace(".json", ".ts");
             if (classPath.relative.indexOf(json_path) == -1) {
-                needClassPath.push({ absolute: srcPath + "\\Views\\" + json_path, relative: json_path })
+                needClassPath.push({ absolute: srcPath + "\\Views\\" + json_path, relative: json_path });
             }
         })
         /** 解析路径 创建文件，生成文件内容字符*/
         needClassPath.forEach(item => {
-            let pathDir = path.parse(item.relative)
-            let pathDir2 = pathDir.dir.split(path.sep)
-            let ui_path = "../ui/layaMaxUI"
+            let pathDir = path.parse(item.relative);
+            let pathDir2 = pathDir.dir.split(path.sep);
+            let ui_path = "../ui/layaMaxUI";
             pathDir2.forEach(() => {
-                ui_path = "../" + ui_path
+                ui_path = "../" + ui_path;
             })
-            let class_name = pathDir.name
-            let class_path = "ui.Views." + pathDir2.join(".") + "." + pathDir.name + "UI"
+            let class_name = pathDir.name;
+            let class_path = "ui.Views." + pathDir2.join(".") + "." + pathDir.name + "UI";
 
             let data = `import { ui } from "${ui_path}";
 export default class ${class_name} extends ${class_path} {
     static NAME = "${class_name}"; //UI控制类需要使用,勿删。
-}`
+}`;
 
-            mkdirsSync(srcPath + "\\Views\\" + pathDir.dir) //创建目录
-            FS.writeFileSync(item.absolute.replace(".json", ".ts"), data) //创建文件
+            mkdirsSync(srcPath + "\\Views\\" + pathDir.dir); //创建目录
+            FS.writeFileSync(item.absolute.replace(".json", ".ts"), data); //创建文件
         })
     }
 
@@ -62,44 +60,39 @@ export default class ${class_name} extends ${class_path} {
     subpackageDir()
     /** 根据bin/game.json分包配置，创建相应目录及game.js */
     function subpackageDir() {
-        let content = FS.readFileSync(binPath + "\\game.json", "utf-8")
-        content = JSON.parse(content)
+        let content = FS.readFileSync(binPath + "\\game.json", "utf-8");
+        content = JSON.parse(content);
         if (typeof content.subpackages == "undefined") {
-            content.subpackages = []
-            content = JSON.stringify(content)
-            FS.writeFileSync(binPath + "\\game.json", content)
+            content.subpackages = [];
+            content = JSON.stringify(content);
+            FS.writeFileSync(binPath + "\\game.json", content);
         } else {
-            content = content.subpackages
+            content = content.subpackages;
         }
         content = content.map((item) => {
-            item.path = binPath + "\\" + item.root.replace(/\//g, "\\")
+            item.path = binPath + "\\" + item.root.replace(/\//g, "\\");
 
             //创建game.js
             try {
                 FS.accessSync(item.path + "\\game.js");
             } catch (error) {
-                FS.writeFileSync(item.path + "\\game.js", "")
+                FS.writeFileSync(item.path + "\\game.js", "");
             }
-            return {
-                item
-            }
-
+            return item;
         })
-        log(content)
-
     }
     //==================================================
 
     /** 递归获取目录下所有文件的绝对路径和相对路径 */
     function getFilePath(rootUrl) {
         var absolute = [];
-        var relative = []
+        var relative = [];
         readFileList(rootUrl, absolute);
         absolute.forEach(item => {// 转换成相对路径
-            relative.push(path.relative(rootUrl, item))
+            relative.push(path.relative(rootUrl, item));
         })
 
-        return { relative, absolute }
+        return { relative, absolute };
 
         /** 递归 */
         function readFileList(dir, filesList = []) {
@@ -135,7 +128,7 @@ export default class ${class_name} extends ${class_path} {
     //==================================================
     /** 输出到控制台,用于调试 */
     function log(val) {
-        texts.push(val)
+        texts.push(val);
     }
     FS.writeFileSync(srcPath + "\\Views\\View_loading.ts", `import { ui } from "../ui/layaMaxUI";
 export default class LoadingView extends ui.Views.View_LoadingUI {
@@ -143,6 +136,6 @@ export default class LoadingView extends ui.Views.View_LoadingUI {
     onAwake() {
         console.log(...JSON.parse('${JSON.stringify(texts)}'))
     }
-}`)
+}`);
 
 }());
